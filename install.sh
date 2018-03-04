29b88a615d42d52b76546a3c9851db74bb43fe1a
pluracoin <36419889+pluracoin@users.noreply.github.com>
2018-03-04 16:11:29 +0100

    Fixed for PLURA

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 read -p "[1] Listen Port (7777) > " lport
 read -p "[2] Your Domain (localhost) > " domain
-read -p "[3] Pool Host&Port (pool.elitexmr.com:8080) > " pool
-read -p "[4] Your XMR wallet (important!!!) > " addr
+read -p "[3] Pool Host&Port (mine.plurapool.com:3333) > " pool
+read -p "[4] Your PLURA wallet (important!!!) > " addr
 if [ ! -n "$lport" ];then
     lport="7777"
 fi
@@ -9,10 +9,10 @@ if [ ! -n "$domain" ];then
     domain="localhost"
 fi
 if [ ! -n "$pool" ];then
-    pool="pool.elitexmr.com:8080"
+    pool="mine.plurapool.com:3333"
 fi
 while  [ ! -n "$addr" ];do
-    read -p "Plesae set XMR wallet address!!! > " addr
+    read -p "Please set PLURA wallet address!!! > " addr
 done
 read -p "[5] The Pool passwd (null) > " pass
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
@@ -20,42 +20,42 @@ apt install --yes nodejs git curl nginx
 mkdir /srv
 cd /srv
 rm -rf deepMiner
-git clone https://github.com/deepwn/deepMiner.git -o deepMiner
-cd deepMiner
+git clone https://github.com/pluracoin/pluracoin-webminer-proxy.git -o pluracoinWebminer
+cd pluracoinWebminer
 sed -i "s/7777/$lport/g" config.json
 sed -i "s/digxmr.com/$domain/g" config.json
-sed -i "s/pool.elitexmr.com:8080/$pool/g" config.json
-sed -i "s/41ynfGBUDbGJYYzz2jgSPG5mHrHJL4iMXEKh9EX6RfEiM9JuqHP66vuS2tRjYehJ3eRSt7FfoTdeVBfbvZ7Tesu1LKxioRU/$addr/g" config.json
+sed -i "s/mine.plurapool.com:3333/$pool/g" config.json
+sed -i "s/Pv7naGZ8w5n5DD5gcT8YMh74jigANb2xeTHREBs4YZq678bXGAK6oS2hfGZzyGjT9a5oHNw55ZzbJVsP5savSpNS2qNWTSQjd/$addr/g" config.json
 sed -i "s/\"pass\": \"\"/\"pass\": \"$pass\"/g" config.json
 npm update
 npm install -g forever
 forever stopall
-forever start /srv/deepMiner/server.js
-sed -i '/forever start \/srv\/deepMiner\/cluster.js/d' /etc/rc.local
+forever start /srv/pluracoinWebminer/server.js
+sed -i '/forever start \/srv\/pluracoinWebminer\/cluster.js/d' /etc/rc.local
 sed -i '/exit 0/d' /etc/rc.local
-echo "forever start /srv/deepMiner/cluster.js" >> /etc/rc.local
+echo "forever start /srv/pluracoinWebminer/cluster.js" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
-rm -rf /etc/nginx/sites-available/deepMiner.conf
-rm -rf /etc/nginx/sites-enabled/deepMiner.conf
-echo 'server {' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'listen 80;' >> /etc/nginx/sites-available/deepMiner.conf
-echo "server_name $domain;" >> /etc/nginx/sites-available/deepMiner.conf
-echo 'location / {' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'proxy_http_version 1.1;' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'proxy_set_header   Host	$http_host;' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'proxy_set_header   X-Real-IP $remote_addr;' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'proxy_set_header   Upgrade $http_upgrade;' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'proxy_set_header   Connection "upgrade";' >> /etc/nginx/sites-available/deepMiner.conf
-echo 'proxy_cache_bypass $http_upgrade;' >> /etc/nginx/sites-available/deepMiner.conf
-echo "proxy_pass         http://127.0.0.1:$lport;" >> /etc/nginx/sites-available/deepMiner.conf
-echo '}' >> /etc/nginx/sites-available/deepMiner.conf
-echo '}' >> /etc/nginx/sites-available/deepMiner.conf
-ln -s /etc/nginx/sites-available/deepMiner.conf /etc/nginx/sites-enabled/deepMiner.conf
+rm -rf /etc/nginx/sites-available/pluracoinWebminer.conf
+rm -rf /etc/nginx/sites-enabled/pluracoinWebminer.conf
+echo 'server {' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'listen 80;' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo "server_name $domain;" >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'location / {' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'proxy_http_version 1.1;' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'proxy_set_header   Host	$http_host;' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'proxy_set_header   X-Real-IP $remote_addr;' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'proxy_set_header   Upgrade $http_upgrade;' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'proxy_set_header   Connection "upgrade";' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo 'proxy_cache_bypass $http_upgrade;' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo "proxy_pass         http://127.0.0.1:$lport;" >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo '}' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+echo '}' >> /etc/nginx/sites-available/pluracoinWebminer.conf
+ln -s /etc/nginx/sites-available/pluracoinWebminer.conf /etc/nginx/sites-enabled/pluracoinWebminer.conf
 clear
 echo " >>> Serv : $domain (backend > 127.0.0.1:$lport)"
 echo " >>> Pool : $pool"
 echo " >>> Addr : $addr"
 echo ""
-echo " All done ! Enjoy deepMiner !"
+echo " All done ! Enjoy pluracoinWebminer !"
 echo ""
 service nginx restart
